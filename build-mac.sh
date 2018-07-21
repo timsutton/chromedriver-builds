df09c272e16bb26f10ba2bea99eae782d1679776
Timothy Sutton <tim@synthist.net>
2018-07-20 21:45:14 -0400

    First attempt at mac build script

---
+++ build-mac.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -eu
+
+git clone https://chromium.googlesource.com/chromium/tools/depot_tools
+PATH=$PATH:$(pwd)/depot_tools
+
+mkdir chromium && cd chromium
+fetch --nohooks --no-history chromium
+
+cd src
+
+gn gen out/Release --args="is_debug=false"
+ninja -C out/Release chromedriver
